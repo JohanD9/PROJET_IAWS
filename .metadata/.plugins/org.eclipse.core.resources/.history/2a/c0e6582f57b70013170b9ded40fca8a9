900ae4069417a7b2d93f489c6222699d34897b62
Debat Johan <johan.debat@gmail.com>
2014-03-29 17:33:02 +0100

    ST4 finie

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2a/c0e6582f57b70013170b9ded40fca8a9
@@ -0,0 +1,36 @@
+package gestionST4;
+
+public class Position {
+	private int lat;
+	private int lng;
+	
+	public Position(int lat, int lng) {
+		this.lat = lat;
+		this.lng = lng;
+	}
+
+	public int getLat() {
+		return lat;
+	}
+
+	public void setLat(int lat) {
+		this.lat = lat;
+	}
+
+	public int getLng() {
+		return lng;
+	}
+
+	public void setLng(int lng) {
+		this.lng = lng;
+	}
+
+	@Override
+	public String toString() {
+		return "Position (lat : " + lat + ", lng : " + lng + ")";
+	}
+	
+	
+	
+	
+}
--- ST4/src/gestionST4/main.java
+++ ST4/src/gestionST4/main.java
@@ -9,19 +9,22 @@ import HTTPrequest.LaunchGetHTTP;
 
 public class main {
 
-	public static void main(String[] args) throws IOException, JSONException {
-		String urlStationPaulSab = "https://api.jcdecaux.com/vls/v1/stations/227?"
-				+ "contract=Toulouse&apiKey=d3a6958a033987763c45e1311847f98909303f34";
-		String jSonPaulSab;
-
-		LaunchGetHTTP getStationPaulSab = new LaunchGetHTTP(urlStationPaulSab);
-		jSonPaulSab = getStationPaulSab.get();
+	public static void main(String[] args) throws NumberFormatException, IOException, JSONException {
 		
-		ParserJsonStation parserStationPaulSab = new ParserJsonStation(jSonPaulSab);
-		Station PaulSab = parserStationPaulSab.parse();
+		if (args.length != 0) {
+			GestionVelo monGestionnaire = new GestionVelo(Integer.parseInt(args[0]));
+			int nbVelo = monGestionnaire.getNbVelosDispo();
+			
+			if (nbVelo == -1) {
+				System.out.println("Aucune station correspondante au numéro entré");
+			} else if (nbVelo > 0) {
+				System.out.println("Il y a " + nbVelo + " vélos disponibles");
+			} else {
+				System.out.println("Il n'y a aucun vélos disponible");
+			}
+		} else {
+			System.out.println("Entrez un numéro de station valide  en paramètre");
+		}
 		
-		System.out.println(PaulSab.toString());
-
 	}
-
 }
