cfe592fe154f916d9c90c8784141b15883cd8458
Debat Johan <johan.debat@gmail.com>
2014-03-31 20:46:19 +0200

    ajout de l'interface et modification du jar gestionVelo (ST4 - fini)

---
+++ ST2/src/parserJson/ParserJsonLine.java
@@ -0,0 +1,56 @@
+package parserJson;
+import gestionST2.Destination;
+import gestionST2.Line;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class ParserJsonLine
+{
+	private String json;
+	
+	public ParserJsonLine(String json)
+	{
+		this.json = json;
+	}
+	
+	public List<Line> parse () throws JSONException
+	{
+		int i;
+		List<Line> listeLine = new ArrayList<Line>();
+		Line line;
+		JSONObject innerJsonObjectDeparture;
+		JSONObject innerJsonObjectLine;
+		JSONArray innerJsonArrayDestination;
+		
+		JSONObject jsonObject = new JSONObject(json);
+		JSONObject innerJsonObject = jsonObject.getJSONObject("departures");
+		JSONArray jsonArray = innerJsonObject.getJSONArray("departure");
+		
+		for(i = 0 ; i < (jsonArray.length() - 1) ; i++)
+		{
+			Line line1 = new Line();
+			
+			innerJsonObjectDeparture = jsonArray.getJSONObject(i);
+			innerJsonObjectLine = innerJsonObjectDeparture.getJSONObject("line");
+			
+			line1.setName(innerJsonObjectLine.getString("name"));
+			line1.setShortName(innerJsonObjectLine.getString("shortname"));
+			line1.setNetwork(innerJsonObjectLine.getString("network"));
+			line1.setColor(innerJsonObjectLine.getString("color"));
+			
+			listeLine.add(line1);
+			
+			innerJsonArrayDestination = null;
+			innerJsonObjectDeparture = null;
+			line1 = null;
+		}
+		
+		return (listeLine);
+	}
+}
